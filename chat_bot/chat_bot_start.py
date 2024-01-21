116fc9aaad9192b3cd82e2e404c5d2806c8d0401
Anton <AntonTishchenk0@yandex.ru>
2024-01-21 20:03:38 +0200

    added chat-bot ver 2

--- chat_bot/chat_bot_start.py
+++ chat_bot/chat_bot_start.py
@@ -4,11 +4,9 @@ from googletrans import Translator
 from langdetect import detect
 import sqlite3
 
-
 API_TOKEN = '6028101102:AAFeWXaKdb9txqyVVbglcGQVRnQOCTVr_MM'
 
 bot = telebot.TeleBot(API_TOKEN)
-
 
 translator = Translator()
 
@@ -116,7 +114,7 @@ def next_question(mess):
 
 
 def get_amswer(mess):
-    my_lst = []
+    """Функция проверки правильности ответа"""
     connection = sqlite3.Connection('my_database.db')
     cursor = connection.cursor()
 
@@ -130,8 +128,357 @@ def get_amswer(mess):
         bot.send_message(mess.chat.id, 'Не правильно!')
     elif mess.text == f'{question[0][3]}':
         bot.send_message(mess.chat.id, 'Не правильно!')
-    elif mess.text == f'{question[0][3]}':
+    elif mess.text == f'{question[0][4]}':
         bot.send_message(mess.chat.id, 'Верный ответ!')
 
+    bot.register_next_step_handler(mess, next_question2)
+
+
+def next_question2(mess):
+    """Функция вывода ответов для пользователя"""
+    connection = sqlite3.Connection('my_database.db')
+    cursor = connection.cursor()
+
+    cursor.execute('SELECT * FROM Testing')
+    question = cursor.fetchall()
+
+    cursor.close()
+    connection.close()
+    bot.send_message(mess.chat.id, f'Вопрос № {question[1][1]}')
+
+    button = types.ReplyKeyboardMarkup()
+    btnA = types.KeyboardButton(f'{question[1][2]}')
+    btnB = types.KeyboardButton(f'{question[1][3]}')
+    btnC = types.KeyboardButton(f'{question[1][4]}')
+    button.add(btnA)
+    button.add(btnB)
+    button.add(btnC)
+    bot.send_message(mess.chat.id, 'Выберите правильный ответ', reply_markup=button)
+    bot.register_next_step_handler(mess, get_amswer2)
+
+
+def get_amswer2(mess):
+    """Функция проверки правильности ответа"""
+    connection = sqlite3.Connection('my_database.db')
+    cursor = connection.cursor()
+
+    cursor.execute('SELECT * FROM Testing')
+    question = cursor.fetchall()
+
+    cursor.close()
+    connection.close()
+
+    if mess.text == f'{question[1][2]}':
+        bot.send_message(mess.chat.id, 'Не правильно!')
+    elif mess.text == f'{question[1][3]}':
+        bot.send_message(mess.chat.id, 'Верный ответ!')
+    elif mess.text == f'{question[1][4]}':
+        bot.send_message(mess.chat.id, 'Не правильно!')
+
+    bot.register_next_step_handler(mess, next_question3)
+
+
+def next_question3(mess):
+    """Функция вывода ответов для пользователя"""
+    connection = sqlite3.Connection('my_database.db')
+    cursor = connection.cursor()
+
+    cursor.execute('SELECT * FROM Testing')
+    question = cursor.fetchall()
+
+    cursor.close()
+    connection.close()
+    bot.send_message(mess.chat.id, f'Вопрос № {question[2][1]}')
+
+    button = types.ReplyKeyboardMarkup()
+    btnA = types.KeyboardButton(f'{question[2][2]}')
+    btnB = types.KeyboardButton(f'{question[2][3]}')
+    btnC = types.KeyboardButton(f'{question[2][4]}')
+    button.add(btnA)
+    button.add(btnB)
+    button.add(btnC)
+    bot.send_message(mess.chat.id, 'Выберите правильный ответ', reply_markup=button)
+    bot.register_next_step_handler(mess, get_amswer3)
+
+
+def get_amswer3(mess):
+    """Функция проверки правильности ответа"""
+    connection = sqlite3.Connection('my_database.db')
+    cursor = connection.cursor()
+
+    cursor.execute('SELECT * FROM Testing')
+    question = cursor.fetchall()
+
+    cursor.close()
+    connection.close()
+
+    if mess.text == f'{question[2][2]}':
+        bot.send_message(mess.chat.id, 'Верный ответ!')
+    elif mess.text == f'{question[2][3]}':
+        bot.send_message(mess.chat.id, 'Не правильно!')
+    elif mess.text == f'{question[2][4]}':
+        bot.send_message(mess.chat.id, 'Не правильно!')
+
+    bot.register_next_step_handler(mess, next_question4)
+
+
+def next_question4(mess):
+    """Функция вывода ответов для пользователя"""
+    connection = sqlite3.Connection('my_database.db')
+    cursor = connection.cursor()
+
+    cursor.execute('SELECT * FROM Testing')
+    question = cursor.fetchall()
+
+    cursor.close()
+    connection.close()
+    bot.send_message(mess.chat.id, f'Вопрос № {question[3][1]}')
+
+    button = types.ReplyKeyboardMarkup()
+    btnA = types.KeyboardButton(f'{question[3][2]}')
+    btnB = types.KeyboardButton(f'{question[3][3]}')
+    btnC = types.KeyboardButton(f'{question[3][4]}')
+    button.add(btnA)
+    button.add(btnB)
+    button.add(btnC)
+    bot.send_message(mess.chat.id, 'Выберите правильный ответ', reply_markup=button)
+    bot.register_next_step_handler(mess, get_amswer4)
+
+
+def get_amswer4(mess):
+    """Функция проверки правильности ответа"""
+    connection = sqlite3.Connection('my_database.db')
+    cursor = connection.cursor()
+
+    cursor.execute('SELECT * FROM Testing')
+    question = cursor.fetchall()
+
+    cursor.close()
+    connection.close()
+
+    if mess.text == f'{question[3][2]}':
+        bot.send_message(mess.chat.id, 'Верный ответ!')
+    elif mess.text == f'{question[3][3]}':
+        bot.send_message(mess.chat.id, 'Не правильно!')
+    elif mess.text == f'{question[3][4]}':
+        bot.send_message(mess.chat.id, 'Не правильно!')
+
+    bot.register_next_step_handler(mess, next_question5)
+
+
+def next_question5(mess):
+    """Функция вывода ответов для пользователя"""
+    connection = sqlite3.Connection('my_database.db')
+    cursor = connection.cursor()
+
+    cursor.execute('SELECT * FROM Testing')
+    question = cursor.fetchall()
+
+    cursor.close()
+    connection.close()
+    bot.send_message(mess.chat.id, f'Вопрос № {question[4][1]}')
+
+    button = types.ReplyKeyboardMarkup()
+    btnA = types.KeyboardButton(f'{question[4][2]}')
+    btnB = types.KeyboardButton(f'{question[4][3]}')
+    btnC = types.KeyboardButton(f'{question[4][4]}')
+    button.add(btnA)
+    button.add(btnB)
+    button.add(btnC)
+    bot.send_message(mess.chat.id, 'Выберите правильный ответ', reply_markup=button)
+    bot.register_next_step_handler(mess, get_amswer5)
+
+
+def get_amswer5(mess):
+    """Функция проверки правильности ответа"""
+    connection = sqlite3.Connection('my_database.db')
+    cursor = connection.cursor()
+
+    cursor.execute('SELECT * FROM Testing')
+    question = cursor.fetchall()
+
+    cursor.close()
+    connection.close()
+
+    if mess.text == f'{question[4][2]}':
+        bot.send_message(mess.chat.id, 'Верный ответ!')
+    elif mess.text == f'{question[4][3]}':
+        bot.send_message(mess.chat.id, 'Не правильно!')
+    elif mess.text == f'{question[4][4]}':
+        bot.send_message(mess.chat.id, 'Не правильно!')
+
+    bot.register_next_step_handler(mess, next_question6)
+
+def next_question6(mess):
+    """Функция вывода ответов для пользователя"""
+    connection = sqlite3.Connection('my_database.db')
+    cursor = connection.cursor()
+
+    cursor.execute('SELECT * FROM Testing')
+    question = cursor.fetchall()
+
+    cursor.close()
+    connection.close()
+    bot.send_message(mess.chat.id, f'Вопрос № {question[5][1]}')
+
+    button = types.ReplyKeyboardMarkup()
+    btnA = types.KeyboardButton(f'{question[5][2]}')
+    btnB = types.KeyboardButton(f'{question[5][3]}')
+    btnC = types.KeyboardButton(f'{question[5][4]}')
+    button.add(btnA)
+    button.add(btnB)
+    button.add(btnC)
+    bot.send_message(mess.chat.id, 'Выберите правильный ответ', reply_markup=button)
+    bot.register_next_step_handler(mess, get_amswer6)
+
+
+def get_amswer6(mess):
+    """Функция проверки правильности ответа"""
+    connection = sqlite3.Connection('my_database.db')
+    cursor = connection.cursor()
+
+    cursor.execute('SELECT * FROM Testing')
+    question = cursor.fetchall()
+
+    cursor.close()
+    connection.close()
+
+    if mess.text == f'{question[5][2]}':
+        bot.send_message(mess.chat.id, 'Верный ответ!')
+    elif mess.text == f'{question[5][3]}':
+        bot.send_message(mess.chat.id, 'Не правильно!')
+    elif mess.text == f'{question[5][4]}':
+        bot.send_message(mess.chat.id, 'Не правильно!')
+
+    bot.register_next_step_handler(mess, next_question7)
+
+def next_question7(mess):
+    """Функция вывода ответов для пользователя"""
+    connection = sqlite3.Connection('my_database.db')
+    cursor = connection.cursor()
+
+    cursor.execute('SELECT * FROM Testing')
+    question = cursor.fetchall()
+
+    cursor.close()
+    connection.close()
+    bot.send_message(mess.chat.id, f'Вопрос № {question[6][1]}')
+
+    button = types.ReplyKeyboardMarkup()
+    btnA = types.KeyboardButton(f'{question[6][2]}')
+    btnB = types.KeyboardButton(f'{question[6][3]}')
+    btnC = types.KeyboardButton(f'{question[6][4]}')
+    button.add(btnA)
+    button.add(btnB)
+    button.add(btnC)
+    bot.send_message(mess.chat.id, 'Выберите правильный ответ', reply_markup=button)
+    bot.register_next_step_handler(mess, get_amswer7)
+
+
+def get_amswer7(mess):
+    """Функция проверки правильности ответа"""
+    connection = sqlite3.Connection('my_database.db')
+    cursor = connection.cursor()
+
+    cursor.execute('SELECT * FROM Testing')
+    question = cursor.fetchall()
+
+    cursor.close()
+    connection.close()
+
+    if mess.text == f'{question[6][2]}':
+        bot.send_message(mess.chat.id, 'Верный ответ!')
+    elif mess.text == f'{question[6][3]}':
+        bot.send_message(mess.chat.id, 'Не правильно!')
+    elif mess.text == f'{question[6][4]}':
+        bot.send_message(mess.chat.id, 'Не правильно!')
+
+    bot.register_next_step_handler(mess, next_question8)
+
+def next_question8(mess):
+    """Функция вывода ответов для пользователя"""
+    connection = sqlite3.Connection('my_database.db')
+    cursor = connection.cursor()
+
+    cursor.execute('SELECT * FROM Testing')
+    question = cursor.fetchall()
+
+    cursor.close()
+    connection.close()
+    bot.send_message(mess.chat.id, f'Вопрос № {question[7][1]}')
+
+    button = types.ReplyKeyboardMarkup()
+    btnA = types.KeyboardButton(f'{question[7][2]}')
+    btnB = types.KeyboardButton(f'{question[7][3]}')
+    btnC = types.KeyboardButton(f'{question[7][4]}')
+    button.add(btnA)
+    button.add(btnB)
+    button.add(btnC)
+    bot.send_message(mess.chat.id, 'Выберите правильный ответ', reply_markup=button)
+    bot.register_next_step_handler(mess, get_amswer8)
+
+
+def get_amswer8(mess):
+    """Функция проверки правильности ответа"""
+    connection = sqlite3.Connection('my_database.db')
+    cursor = connection.cursor()
+
+    cursor.execute('SELECT * FROM Testing')
+    question = cursor.fetchall()
+
+    cursor.close()
+    connection.close()
+
+    if mess.text == f'{question[7][2]}':
+        bot.send_message(mess.chat.id, 'Верный ответ!')
+    elif mess.text == f'{question[7][3]}':
+        bot.send_message(mess.chat.id, 'Не правильно!')
+    elif mess.text == f'{question[7][4]}':
+        bot.send_message(mess.chat.id, 'Не правильно!')
+
+    bot.register_next_step_handler(mess, next_question9)
+
+def next_question9(mess):
+    """Функция вывода ответов для пользователя"""
+    connection = sqlite3.Connection('my_database.db')
+    cursor = connection.cursor()
+
+    cursor.execute('SELECT * FROM Testing')
+    question = cursor.fetchall()
+
+    cursor.close()
+    connection.close()
+    bot.send_message(mess.chat.id, f'Вопрос № {question[8][1]}')
+
+    button = types.ReplyKeyboardMarkup()
+    btnA = types.KeyboardButton(f'{question[8][2]}')
+    btnB = types.KeyboardButton(f'{question[8][3]}')
+    btnC = types.KeyboardButton(f'{question[8][4]}')
+    button.add(btnA)
+    button.add(btnB)
+    button.add(btnC)
+    bot.send_message(mess.chat.id, 'Выберите правильный ответ', reply_markup=button)
+    bot.register_next_step_handler(mess, get_amswer9)
+
+
+def get_amswer9(mess):
+    """Функция проверки правильности ответа"""
+    connection = sqlite3.Connection('my_database.db')
+    cursor = connection.cursor()
+
+    cursor.execute('SELECT * FROM Testing')
+    question = cursor.fetchall()
+
+    cursor.close()
+    connection.close()
+
+    if mess.text == f'{question[7][2]}':
+        bot.send_message(mess.chat.id, 'Верный ответ!')
+    elif mess.text == f'{question[7][3]}':
+        bot.send_message(mess.chat.id, 'Не правильно!')
+    elif mess.text == f'{question[7][4]}':
+        bot.send_message(mess.chat.id, 'Не правильно!')
+
+    bot.register_next_step_handler(mess, started)
 
 bot.polling(none_stop=True)
